d76dc491991941054ff2ba8733e0814b1fd8de6a
Oscar David Arbeláez <odarbelaeze@gmail.com>
2013-02-27 10:07:11 -0500

    Sponsors added.

--- common/footer.php
+++ common/footer.php
@@ -1,11 +1,9 @@
 <div class="container">
     <div class="sponsors" id="sponsors">
-        <a href="http://www.manizales.unal.edu.co/" class="sponsor" target="_blank" title="Universidad Nacional sede Manizales">
-            <img src="img/sponsors/unal-manizales" alt="Universidad Nacional sede Manizales"></a>
-        <a href="http://www.medellin.unal.edu.co/" class="sponsor" target="_blank" title="Universidad Nacional sede Medellín">
-            <img src="img/sponsors/unal-medellin.png" alt="Universidad Nacional sede Medellín"></a>
+        <a href="http://www.unal.edu.co/" class="sponsor" target="_blank" title="Universidad Nacional de Colombia">
+            <img src="img/sponsors/Logo-UN.png" alt="Universidad Nacional de Colombia"></a>
         <a href="http://168.176.145.23/" class="sponsor" target="_blank" title="PCM Computational Applications">
-            <img src="img/sponsors/Logo-PCM.png" alt="PCM Computational Applications"></a>
+            <img src="img/sponsors/ShadedLogo.png" alt="PCM Computational Applications"></a>
         <a href="http://sec.kingston.ac.uk/research/research-centres/digital-imaging-research-centre/" class="sponsor" target="_blank" title="Kingston University of London">
             <img src="img/sponsors/kingston.png" alt="Kingston University of London"></a>
         <a href="http://www.elec.qmul.ac.uk/mmv/" class="sponsor" target="_blank" title="Queen Mary University of London">
--- program.php
+++ program.php
@@ -18,9 +18,67 @@
             <div class="page-header"><h2>Keynote Speakers</h2></div>
             <article class="keynoteSpeaker">
                 <h3>Aggelos Katsaggelos <small>Northwestern University (US)</small></h3>
+                <p>
+                    <strong>Aggelos K. Katsaggelos</strong> received the Diploma degree in electrical and mechanical 
+                    engineering from the Aristotelian University of Thessaloniki, Thessaloniki, 
+                    Greece, in 1979 and the M.S. and Ph.D. degrees both in electrical engineering 
+                    from the Georgia Institute of Technology, Atlanta, Georgia, in 1981 and 1985, 
+                    respectively. In 1985 he joined the Department of Electrical Engineering and 
+                    Computer Science at Northwestern University, Evanston, IL, where he is currently 
+                    professor, holder of the AT&amp;T Chair (previously holder of the Ameritech Chair). 
+                    He is also the Director of the Motorola Center for Seamless Communications, an 
+                    Academic Affiliate Staff, Department of Medicine, at NorthShore University 
+                    HealthCare System, an affiliated faculty at the Department of Linguistics, 
+                    Northwestern University, and an appointee at the Argonne National Laboratory. 
+                    During the 1986-1987 academic year he was an assistant professor at Polytechnic 
+                    University, Department of Electrical Engineering and Computer Science, Brooklyn, 
+                    NY. His current research interests include multimedia signal processing (e.g. 
+                    image and video recovery and compression, audio-visual speech and speaker recognition, 
+                    indexing and retrieval), multimedia communications, computer vision, pattern recognition, 
+                    and DNA signal processing. He has published extensively in these areas. He is the 
+                    editor of Digital Image Restoration (Springer-Verlag 1991), co-author of Rate-Distortion 
+                    Based Video Compression (Kluwer 1997), co-editor of Recovery Techniques for Image 
+                    and Video Compression and Transmission, (Kluwer 1998), and co-author of Super-Resolution 
+                    of Images and Video (Morgan &amp; Claypool Publishers 2007) and Joint Source-Channel Video 
+                    Transmission (Morgan &amp; Claypool Publishers 2007). Prof. Katsaggelos has served in the 
+                    IEEE and other professional Societies in many capacities. He is currently a member of 
+                    the IEEE Technical Committee on Visual Signal Processing and Communications, and the 
+                    Editorial Boards of Academic Press, Marcel Dekker: Signal Processing Series, the 
+                    International Journal on Image and Video Processing, and Advances in Multimedia. He 
+                    has served as editor-in-chief of the IEEE Signal Processing Magazine (1997-2002), a 
+                    member of the Publication Boards of the IEEE Signal Processing Society, the IEEE TAB 
+                    Magazine Committee, an Associate editor for the IEEE Transactions on Signal Processing 
+                    (1990-1992), an area editor for the journal Graphical Models and Image Processing 
+                    (1992-1995), a member of the Steering Committees of the IEEE Transactions on Image 
+                    Processing (1992-1997) and the IEEE Transactions on Medical Imaging (1990-1999), a 
+                    member of the IEEE Technical Committee on Image and Multi-Dimensional Signal Processing 
+                    (1992-1998), a member of the Board of Governors of the IEEE Signal Processing Society 
+                    (1999-2001), a member of the IEEE Technical Committees on Multimedia Signal Processing 
+                    and Image and Multidimensional Signal Processing, a member of the Publication Board of 
+                    the IEEE Proceedings (2003-2007), and a member of the Editorial Boards of Applied Signal 
+                    Processing and Computer Journal. He has served as the General Chairman of the 1994 
+                    Visual Communications and Image Processing Conference (Chicago, IL) and as technical 
+                    program co-chair of the 1998 IEEE International Conference on Image Processing 
+                    (Chicago, IL). Dr. Katsaggelos is a Fellow of the IEEE (1998) and SPIE (2009), the 
+                    co-inventor of seventeen international patents, the recipient of the IEEE Third Millennium 
+                    Medal (2000), the IEEE Signal Processing Society Meritorious Service Award (2001), the 
+                    IEEE Signal Processing Society Technical Achievement Award (2010), and a co-recipient of 
+                    an IEEE Signal Processing Society Best Paper Award (2001), an IEEE ICME Best Paper Award 
+                    (2006), an IEEE ICIP Best Paper Award (2007), and an ISPA Best Paper Award (2009). He was
+                    a Distinguished Lecturer of the IEEE Signal Processing Society (2007-08).
+                </p>
             </article>
             <article class="keynoteSpeaker">
                 <h3>Ebroul Izquierdo <small>Queen Mary University of London (UK)</small></h3>
+                <p>
+                    <strong>Ebroul Izquierdo, PhD, MSc, CEng, FIET, SMIEEE, MBMVA</strong>, is Chair of Multimedia 
+                    and Computer Vision and head of the Multimedia and Vision Group in the school of Electronic 
+                    Engineering and Computer Science at Queen Mary, University of London. For his thesis on the 
+                    numerical approximation of algebraic-differential equations, he received the Dr. Rerum 
+                    Naturalium (PhD) from the Humboldt University, Berlin, Germany. He has been a senior researcher 
+                    at the Heinrich-Hertz Institute for Communication Technology (HHI), Berlin, Germany, and the 
+                    Department of Electronic Systems Engineering of the University of Essex.
+                </p>
             </article>
         </section>
         <section id="tutorialSessions">
